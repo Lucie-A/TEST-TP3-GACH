5bec31ee6c6c54e7c45c31ec02d4f7183944c86a
Lucie <luciehello11@gmail.com>
2020-03-21 23:47:51 +0100

    Retrait bouton affiche questions cause d'erreurs

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,33 +1,15 @@
 <template>
-  <div id="listequestions">
+  <div>
       <h2>{{ quiz.titre }}</h2>
       <!--Ajout du nombre de question-->
       Nb Questions :
       {{ listeQuestions ? listeQuestions.length : 0 }}
-      <button @click="titrequestion" v-for="question in listeQuestions" :key="question.id">Question {{ question.id }}</button>
+      <button v-for="question in listeQuestions" :key="question.id">Question {{ question.id }}</button>
   </div>
 </template>
 <script>
 import { getQuiz } from "../data/quiz";
 import { getQuizAjax } from "../data/ajax";
-
-var lesquestions = new Vue({
-  el: '#listequestions',
-  data: {
-    name: 'Vue.js'
-  },
-  // Définissez les méthodes de l'objet
-  methods: {
-    titrequestions: function (event) {
-      // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
-      document.write(<h3>{{ question,titre }}</h3>)
-      // `event` est l'évènement natif du DOM
-      if (event) {
-        alert(event.target.tagName)
-      }
-    }
-  }
-})
 
 export default {
   name: "Quiz",
